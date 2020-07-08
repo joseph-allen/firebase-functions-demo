721361910ab42f1062254cd426299aa9806261b0
Joe RN <joseph.allen.1994@hotmail.com>
2020-07-08 16:19:49 +0100

    Handle user login and get access token

---
+++ dbschema.js
@@ -0,0 +1,11 @@
+let db = {
+  screams: [
+    {
+      userHandle: "user",
+      body: "this is the scream body",
+      createdAt: "2020-07-08T14:31:30.761Z",
+      likeCount: 5,
+      commentCount: 2,
+    },
+  ],
+};
--- functions/index.js
+++ functions/index.js
@@ -2,7 +2,23 @@ const functions = require("firebase-functions");
 const admin = require("firebase-admin");
 const express = require("express");
 const app = express();
+const firebase = require("firebase");
+
 admin.initializeApp();
+const config = {
+  apiKey: "AIzaSyDqOXXofN0ilKwuUIUUJKbdVMNDG2vbiEc",
+  authDomain: "socialape-5c8fd.firebaseapp.com",
+  databaseURL: "https://socialape-5c8fd.firebaseio.com",
+  projectId: "socialape-5c8fd",
+  storageBucket: "socialape-5c8fd.appspot.com",
+  messagingSenderId: "554741402311",
+  appId: "1:554741402311:web:680eb4357b2668659e4ce2",
+  measurementId: "G-32L90R3RK1",
+};
+// Initialize Firebase
+firebase.initializeApp(config);
+
+const db = admin.firestore();
 
 app.get("/helloWorld", (request, response) => {
   response.send("Hello from Firebase!");
@@ -33,7 +49,7 @@ app.post("/scream", (req, res) => {
   const newScream = {
     body: req.body.body,
     userHandle: req.body.userHandle,
-    createdAt: admin.firestore.Timestamp.fromDate(new Date()),
+    createdAt: new Date().toISOString(),
   };
 
   admin
@@ -49,4 +65,36 @@ app.post("/scream", (req, res) => {
     });
 });
 
-exports.api = functions.https.onRequest(app);
+app.post("/signup", (req, res) => {
+  const newUser = {
+    email: req.body.email,
+    password: req.body.password,
+    confirmPassword: req.body.confirmPassword,
+    handle: req.body.handle,
+  };
+
+  //   TODO: validate data
+  db.doc(`/users/${newUser.handle}`)
+    .get()
+    .then((doc) => {
+      if (doc.exists) {
+        return res.status(400).json({ handle: "this handle is already taken" });
+      } else {
+        return firebase
+          .auth()
+          .createUserWithEmailAndPassword(newUser.email, newUser.password);
+      }
+    })
+    .then((data) => {
+      return data.user.getIdToken();
+    })
+    .then((token) => {
+      return res.status(201).json({ token });
+    })
+    .catch((err) => {
+      console.error(err);
+      return res.status(500).json({ error: err.code });
+    });
+});
+
+exports.api = functions.region("europe-west1").https.onRequest(app);
